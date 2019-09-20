5f52332719f2fe6e1a7c8bc8e210a0174ebdd49e
baptisteanorga <baptiste.anorga@yahoo.fr>
2019-09-20 10:42:52 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductGetComponent } from './product-get/product-get.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 
+// app.module.ts
+
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +20,8 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
